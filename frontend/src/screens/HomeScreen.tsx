35afe02f0631a6274d6333dbfe140b3da1bd6635
Mark Larson <mlarson154@gmail.com>
2026-09-09 16:00:18 -0600

    HomeScreen.tsx: classes on h1

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -4,7 +4,7 @@ import ProductCard from "../components/ProductCard";
 const HomeScreen = () => {
     return (
         <>
-            <h1>Latest Products</h1>
+            <h1 className={`text-3xl font-semibold mb-3`}>Latest Products</h1>
             <div className={`grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6`}>
                 {products.map((product:Product) => (
                     <ProductCard key={product._id} product={product} />
