1e703e74608d4289d6100a43d36e36992df88478
Mark Larson <mlarson154@gmail.com>
2026-09-08 18:21:03 -0600

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className={`text-2xl text-center bg-gray-300 px-4 py-2`}>ProShop Ecommerce</h1>
+    </>
+  )
+}
+
+export default App
