d47a2a534e6c0417b04893efa13997f0d723fe2d
Mark Larson <mlarson154@gmail.com>
2026-09-09 10:40:35 -0600

    App.tsx: moved h1 to Header, replaced with Header componenet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className={`text-2xl text-center bg-gray-300 px-4 py-2`}>ProShop Ecommerce</h1>
+        <Header></Header>
     </>
   )
 }
