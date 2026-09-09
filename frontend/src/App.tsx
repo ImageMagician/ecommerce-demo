fc68c82cf6fb6dc90415d3aac3497bbe769919ee
Mark Larson <mlarson154@gmail.com>
2026-09-09 11:10:25 -0600

    App.tsx: added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
-        <Header></Header>
+        <Header />
+        <main className={`p-3`}>
+            <div className="max-w-7xl mx-auto">
+                <h1>Welcome to ProShop.</h1>
+            </div>
+        </main>
+        <Footer />
     </>
   )
 }
