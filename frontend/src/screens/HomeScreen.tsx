3357f35fbfbb9bfdf51f6c4042fd274c82857925
Mark Larson <mlarson154@gmail.com>
2026-09-09 15:26:24 -0600

    screens/HomeScreen.tsx: create

---
+++ frontend/src/screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import products from "../products.ts";
+import type { Product } from "../types.js";
+import ProductCard from "../components/ProductCard";
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest Products</h1>
+            <div className={`grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6`}>
+                {products.map((product:Product) => (
+                    <ProductCard key={product._id} product={product} />
+                ))}
+            </div>
+        </>
+    )
+};
+
+export default HomeScreen;
