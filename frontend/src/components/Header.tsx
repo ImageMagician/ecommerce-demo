9cb792dccee49fb76ecee0f0f63dd9031f15fd59
Mark Larson <mlarson154@gmail.com>
2026-09-09 11:01:31 -0600

    Header.tsx: added menu content

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-// import { faShoppingCart, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 type NavLink = {
     href: string,
@@ -12,29 +13,42 @@ const Header = () => {
     const toggleMenu = () => setIsOpen(!isOpen);
 
     const links: NavLink[] = [
-        { href: "/", label: "Home" },
-        { href: "/about", label: "About" },
-        { href: "/contact", label: "Contact" }
+        { href: "/account", label: "Account" },
+        { href: "/orders", label: "Orders" },
+        { href: "/logout", label: "Logout" },
     ];
 
     return (
         <header className="bg-gray-300">
             <div className="flex items-center justify-between gap-4 max-w-7xl mx-auto">
-                <h1 className={`text-2xl px-4 py-2`}>ProShop Ecommerce</h1>
-                <nav className={`relative`}>
-                    <button onClick={toggleMenu} aria-expanded={isOpen} className={`border-none px-6 py-2 rounded hover:text-blue-500 cursor-pointer`}>
-                        Menu
-                    </button>
-                    {isOpen && (
-                        <ul className={`absolute top-full left-0 border border-gray-300 bg-white rounded shadow`}>
-                            {links.map((link:any) => (
-                                <li key={link.href}>
-                                    <a href={link.href} className={`block px-4 py-2 text-gray-500 hover:text-gray-800 hover:bg-gray-100`}>{link.label}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </nav>
+                <h1 className={`text-2xl px-4 py-2`}>
+                    <a href="/">
+                        ProShop Ecommerce
+                    </a>
+                </h1>
+                <div className="flex items-center justify-between gap-1">
+                    <nav className={`flex gap-2 items-center`}>
+                        <a href="/products" className={`hover:text-blue-600`}>Products</a>
+                        <div className="relative">
+
+                        <button onClick={toggleMenu} aria-expanded={isOpen} className={`border-none px-6 py-2 rounded hover:text-blue-700 cursor-pointer`}>
+                            Account
+                        </button>
+                        {isOpen && (
+                            <ul className={`absolute top-full left-0 border border-gray-300 bg-white rounded shadow`}>
+                                {links.map((link:any) => (
+                                    <li key={link.href}>
+                                        <a href={link.href} className={`block px-4 py-2 text-gray-500 hover:text-gray-800 hover:bg-gray-100`}>{link.label}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                        </div>
+                        <a href="/cart" className="hover:text-blue-700">
+                            <FontAwesomeIcon icon={faShoppingCart} />
+                        </a>
+                    </nav>
+                </div>
             </div>
         </header>
         )
