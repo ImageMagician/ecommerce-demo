f3519c0a9a793c0c44102ff18782a2a01d9c750b
Mark Larson <mlarson154@gmail.com>
2026-09-09 15:26:55 -0600

    ProductCard.tsx: create

---
+++ frontend/src/components/ProductCard.tsx
@@ -0,0 +1,21 @@
+import type {Product} from "../types.ts";
+
+interface ProductCardProps {
+    product: Product;
+}
+
+const ProductCard = ({product}:ProductCardProps) => {
+    return (
+        <a href={`/product/${product._id}`} className="border border-gray-300 p-3 my-3 rounded-md shadow hover:scale-[102%] transition-all">
+            <div className="relative pb-8 h-full">
+                <img src={product.image} alt={product.name} />
+                <div className="pt-3">
+                    <h3 className={`font-semibold mb-2`}>{product.name}</h3>
+                </div>
+                <p className={`block absolute bottom-0 left-0 text-xl text-gray-500 font-semibold`}>${product.price}</p>
+            </div>
+        </a>
+    )
+}
+
+export default ProductCard;
