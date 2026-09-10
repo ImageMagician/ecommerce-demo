eb2e9c988d56790bccb222ba36e18251d2e40cdd
Mark Larson <mlarson154@gmail.com>
2026-09-09 18:06:01 -0600

    ProductDetailScreen.tsx: created

---
+++ frontend/src/screens/ProductDetailScreen.tsx
@@ -0,0 +1,51 @@
+import { useParams } from 'react-router-dom'
+import products from '../../src/products'
+import Rating from "../components/Rating.tsx";
+import { Link } from "react-router-dom";
+
+const ProductDetailScreen = () => {
+    const { id: productId } = useParams();
+    const product = products.find((p) => p._id === productId);
+
+    return (
+        <div>
+            <div className="text-right mb-8">
+                <Link to={'/'} className={`py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white uppercase text-sm font-semibold rounded`}>Back</Link>
+            </div>
+        { product
+            ? <div className="sm:flex sm:gap-8">
+                <div className={`sm:w-1/2`}>
+                    <img src={product.image} alt={product.name} className={`w-full rounded-md`}/>
+                </div>
+                <div className={`sm:w-1/2`}>
+                    <h1 className={`text-3xl font-bold mb-2`}>{product.name}</h1>
+                    <div className={`mb-2`}>
+                        <Rating value={product.rating} text={product.numReviews} />
+                    </div>
+                    <div className="flex gap-4 items-center mb-4">
+                        <div className={`text-4xl font-semibold text-blue-500 leading-tight ${product.countInStock === 0 && 'line-through'}`}>
+                            ${product.price}
+
+                        </div>
+                        { product.countInStock < 10 && product.countInStock > 0
+                            && <span className="text-red-500 ml-2 inline-block">Only {product.countInStock} left in stock!</span>
+                        }
+                    </div>
+                    <p className={`mb-4`}>{product.description}</p>
+                    <div>
+                        <button type="button"
+                                className={`block border-0 rounded-md bg-blue-500 hover:bg-button-600 text-white font-semibold uppercase py-2 px-8 ${!product.countInStock && 'bg-gray-300'}`}
+                                disabled={!product.countInStock}
+                        >
+                            { product.countInStock > 0 ? 'Add to Cart' : 'Out of Stock' }
+                        </button>
+                    </div>
+                </div>
+            </div>
+            : <p>Product not found.</p>
+        }
+        </div>
+    )
+}
+
+export default ProductDetailScreen;
