7809c0c672d7a5c9869e7e7ac8453fcf45ec7688
Mark Larson <mlarson154@gmail.com>
2026-09-11 17:15:59 -0600

    server.js: added connectDB for mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import products from './data/products.ts';
 import dotenv from 'dotenv';
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const port = process.env.PORT || "http://localhost:8000";
+
+connectDB();
 
 const app = express();
 
