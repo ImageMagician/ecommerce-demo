ef3863fd9cf71a3e87518a94df7a0dadc82ab7ce
Mark Larson <mlarson154@gmail.com>
2026-09-11 17:15:26 -0600

    backend/db.js: added dns fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 import mongoose from 'mongoose'
+import dns from 'dns'
+
+// fixes dns issue connecting to mongodb atlas
+dns.setServers(['8.8.8.8', '1.1.1.1'])
 
 const connectDB = async () => {
     try {
