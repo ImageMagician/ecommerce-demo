25e1096bf609a0d042618330782ab7cf9529a5ae
Mark Larson <mlarson154@gmail.com>
2026-09-11 18:51:49 -0600

    server.js: updated products.ts to .js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import products from './data/products.ts';
+import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
 
