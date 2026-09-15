f836b4452d8de73aa2b5cd5b28859253846ec95c
Mark Larson <mlarson154@gmail.com>
2026-09-14 20:30:46 -0600

    productRoutes.js: changed routes to use productController.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,22 +1,9 @@
 import express from 'express';
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import {getProducts, getProductById } from '../controllers/productController.js';
 
 const router = express.Router();
 
-router.get('/', asyncHandler( async( req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-}));
-
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (!product) {
-        throw new Error('This Product was not found or no longer exists');
-    }
-    res.json(product);
-
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
