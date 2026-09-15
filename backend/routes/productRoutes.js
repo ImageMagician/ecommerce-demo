4406c63e5083aaeaea0a760f2cbf6138ae57eb5a
Mark Larson <mlarson154@gmail.com>
2026-09-14 19:45:18 -0600

    productRoutes.js: created and moved product routes from server.js

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import asyncHandler from '../middleware/asyncHandler.js';
+
+const router = express.Router();
+
+router.get('/', asyncHandler( async( req, res) => {
+    res.json(products);
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+}));
+
+export default router;
