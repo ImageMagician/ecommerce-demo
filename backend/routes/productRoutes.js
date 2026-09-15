1b1ae90a68d129fe9ecab6bc2cff0a0a5848dd03
Mark Larson <mlarson154@gmail.com>
2026-09-14 19:48:13 -0600

    productRoutes.js: imported productModel for mongoose pull of products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
 
 const router = express.Router();
 
 router.get('/', asyncHandler( async( req, res) => {
+    const products = await Product.find({});
     res.json(products);
 }));
 
 router.get('/:id', asyncHandler(async (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
+    const product = await Product.findById(req.params.id);
     res.json(product);
 }));
 
