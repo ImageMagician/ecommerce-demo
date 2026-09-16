121a5715250a8ab6b646d9ef46dcf3ccaef13bac
Mark Larson <mlarson154@gmail.com>
2026-09-15 21:07:07 -0600

    Header.tsx: styles for responsive design

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faBars } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
 
 type NavLink = {
@@ -10,8 +10,16 @@ type NavLink = {
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [mobileOpen, setMobileOpen] = useState(false);
 
     const toggleMenu = () => setIsOpen(!isOpen);
+
+    const mobileMenuToggle = () => {
+        if (mobileOpen) {
+            setIsOpen(true);
+        }
+        setMobileOpen(!mobileOpen);
+    }
 
     const links: NavLink[] = [
         { href: "/account", label: "Account" },
@@ -20,7 +28,7 @@ const Header = () => {
     ];
 
     return (
-        <header className="border-b border-gray-200">
+        <header className="sticky top-0 bg-white border-b border-gray-200">
             <div className="flex items-center justify-between gap-4 max-w-7xl mx-auto">
                 <h1 className={`text-2xl px-4 py-4`}>
                     <Link to="/">
@@ -28,26 +36,38 @@ const Header = () => {
                         <img src="/ts-logo.svg" alt="TechStash" className={`md:hidden w-8 h-auto`} />
                     </Link>
                 </h1>
-                <div className="flex items-center justify-between gap-1">
-                    <nav className={`flex gap-2 items-center`}>
-                        <Link to="/products" className={`hover:text-blue-600`}>Products</Link>
-                        <div className="relative">
-
-                        <button onClick={toggleMenu} aria-expanded={isOpen} className={`border-none px-6 py-2 rounded hover:text-blue-700 cursor-pointer`}>
-                            Account
-                        </button>
-                        {isOpen && (
-                            <ul className={`absolute top-full left-0 border border-gray-300 bg-white rounded shadow`}>
-                                {links.map((link:any) => (
-                                    <li key={link.href}>
-                                        <Link to={link.href} className={`block px-4 py-2 text-gray-500 hover:text-gray-800 hover:bg-gray-100`}>{link.label}</Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        )}
+                <div className="flex flex-col sm:flex-row items-center justify-end gap-1 px-2">
+                    <button className={`${mobileOpen ? 'hidden' : 'block'} ml-auto sm:hidden border-2 border-gray-500 hover:border-gray-700 text-gray-500 hover:text-gray-700 rounded px-0.5`}
+                            onClick={mobileMenuToggle}
+                    >
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                    <nav className={`fixed top-0 bottom-0 right-0 p-3 bg-blue-900/85 sm:bg-transparent text-white sm:text-gray-500 w-auto transition-all duration-300 sm:static sm:block sm:flex gap-2 items-center ${mobileOpen ? 'left-0' : 'left-full'}`}>
+                        <div className={`text-end mb-1 block sm:hidden`}>
+                            <button className={`block ml-auto border-2 border-white rounded px-1.5`}
+                                    onClick={mobileMenuToggle}
+                            >
+                                &times;
+                            </button>
                         </div>
-                        <Link to="/cart" className="hover:text-blue-700">
+                        <Link to="/products" className={`w-full sm:w-auto hover:text-blue-600 inline-block p-2 border-b border-b-white/30 sm:border-b-0`}>Products</Link>
+                        <div className="relative">
+                            <button onClick={toggleMenu} aria-expanded={isOpen} className={`border-b border-b-white/30 w-full sm:w-auto text-start sm:border-none p-2 sm:rounded hover:text-blue-700 cursor-pointer`}>
+                                Account
+                            </button>
+                            {isOpen && (
+                                <ul className={`sm:absolute sm:top-full sm:left-0 sm:border sm:border-gray-300 sm:bg-white sm:rounded sm:shadow`}>
+                                    {links.map((link:any) => (
+                                        <li key={link.href}>
+                                            <Link to={link.href} className={`block pl-6 pr-4 sm:px-4 py-2 text-white border-b border-b-white/30 sm:border-0 sm:text-gray-500 hover:text-gray-800 hover:bg-gray-100`}>{link.label}</Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                        <Link to="/cart" className="hover:text-blue-700 p-2 sm:p-0 inline-block">
                             <FontAwesomeIcon icon={faShoppingCart} />
+                            <span className="sm:hidden ml-2">Shopping Cart</span>
                         </Link>
                     </nav>
                 </div>
