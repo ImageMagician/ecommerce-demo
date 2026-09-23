d52ead7f04a53d63c7d95d4b82f4b49d340b37fd
Mark Larson <mlarson154@gmail.com>
2026-09-23 16:11:45 -0600

    Message.tsx: changed so text content is within tags.

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,3 +1,5 @@
+import type { ReactNode } from "react";
+
 const variantStyles = {
     info: "bg-blue-100 text-blue-800 border-blue-600",
     success: "bg-green-100 text-green-800 border-green-600",
@@ -8,15 +10,15 @@ type MessageVariant = "info" | "success" | "error"
 
 interface MessageProps {
     classes?: string;
-    message?: string;
     variant?: MessageVariant;
+    children?: ReactNode;
 }
 
-const Message = ({ classes="", message, variant="info"}:MessageProps) => {
+const Message = ({ classes="", variant="info", children }:MessageProps) => {
 
     return (
         <div className={`p-4 rounded-md shadow mb-4 border ${variantStyles[variant]} ${classes}`}>
-            { message }
+            { children }
         </div>
     )
 }
