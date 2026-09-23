0e154bbfa771467673a42492e03ccfa9319f3f6a
Mark Larson <mlarson154@gmail.com>
2026-09-23 16:28:15 -0600

    Footer.tsx: changed company name to env var

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className={`bg-black p-3 text-white`}>
             <div className="max-w-7xl mx-auto">
                 <p>
-                    ProShop &copy; {currentYear}
+                    { import.meta.env.VITE_APP_NAME } &copy; {currentYear}
                 </p>
             </div>
         </footer>
