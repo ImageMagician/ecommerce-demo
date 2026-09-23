7c511f7c302edea29c9737b45c9f37d532e46100
Mark Larson <mlarson154@gmail.com>
2026-09-23 16:11:58 -0600

    QtyPicker.tsx: changed styles

--- frontend/src/components/QtyPicker.tsx
+++ frontend/src/components/QtyPicker.tsx
@@ -10,12 +10,12 @@ interface QtyProps {
 
 const QtyPicker = ( { product, qty, addQty, subtractQty, changeQty  }: QtyProps ) => {
     return (
-            <div id="qty" className={`flex gap-1 p-2 rounded-md bg-gray-200 ${ product.countInStock === 0 && 'hidden' }`}>
+            <div id="qty" className={`flex items-center rounded-md ${ product.countInStock === 0 && 'hidden' }`}>
                 <div className="leading-9 text-sm uppercase mr-2 text-gray-600">
                     QTY:
                 </div>
                 <button type="button"
-                        className="bg-blue-500 hover:bg-blue-600 text-white uppercase text-sm font-semibold rounded px-2 py-1 disabled:bg-gray-300 cursor-pointer"
+                        className="bg-blue-500 hover:bg-blue-600 text-white uppercase text-sm font-semibold rounded-l-lg px-1.5 py-0.5 disabled:bg-gray-300 cursor-pointer"
                         disabled={ qty === 1 }
                         onClick={subtractQty}
                 >
@@ -23,11 +23,11 @@ const QtyPicker = ( { product, qty, addQty, subtractQty, changeQty  }: QtyProps 
                 </button>
                 <input type="text"
                        value={qty}
-                       className="border border-gray-200 bg-white px-2 py-1 w-10 text-center rounded"
+                       className="border border-gray-200 bg-white  py-1 w-10 text-center text-xs"
                        onChange={ e => changeQty( parseInt( e.currentTarget.value, 10 ) ) }
                 />
                 <button type="button"
-                        className="bg-blue-500 hover:bg-blue-600 text-white uppercase text-sm font-semibold rounded px-2 py-1 disabled:bg-gray-300 cursor-pointer"
+                        className="bg-blue-500 hover:bg-blue-600 text-white uppercase text-sm font-semibold rounded-r-lg px-1.5 py-0.5 disabled:bg-gray-300 cursor-pointer"
                         onClick={addQty}
                         disabled={ qty >= product.countInStock }
                 >
